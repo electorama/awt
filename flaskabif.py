a82fe758a2b4ab82ec0e43f0d13fe3d0575c721b
Rob Lanphier <roblan@gmail.com>
2023-11-28 04:46:24 +0000

    Framework for flask frontend to abiftool.  Still crashes

--- flaskabif.py
+++ flaskabif.py
@@ -1,20 +1,41 @@
 from flask import Flask, render_template, request
+from pathlib import Path
 
 app = Flask(__name__)
 
+TESTFILEDIR='/home/robla/abiftool/testdata'
+# EXAMPLEFILENAME='burl2009/burl2009.abif'
+EXAMPLEFILENAME='tennessee-example/tennessee-example-scores.abif'
+example_abif = Path(TESTFILEDIR, EXAMPLEFILENAME).read_text()
+from abiflib import htmltable_pairwise_and_winlosstie
+
 @app.route('/', methods=['GET', 'POST'])
 def index():
+    # abiftool.py -f abif -t html_snippet abiftool/testdata/burl2009/burl2009.abif
     if request.method == 'POST':
+        submission_title = '(unknown abif title)'
+        thisformtitle = f'abiftool for the web {submission_title}'
         abifinput = request.form['abifinput']
+        #abifout = htmltable_pairwise_and_winlosstie(abifinput,
+        #                          snippet = True,
+        #                          validate = True,
+        #                          modlimit = 2500)
+        abifout = "<hr>FIXMEsrsly<hr>"
+
         return render_template('index.html',
                                abifinput=abifinput,
-                               formtitle='moo',
-                               abiftool_output='moo23')
+                               formtitle=thisformtitle,
+                               abiftool_output=abifout,
+                               abif_to_display=abifinput)
     else:
+        submission_title = '(empty form)'
+        thisformtitle = f'abiftool for the web {submission_title}'
         return render_template('index.html',
                                abifinput='',
-                               formtitle='moo',
-                               abiftool_output='moo22')
+                               formtitle=thisformtitle,
+                               abiftool_output='(no output yet FIXME)',
+                               abif_to_display=example_abif)
 
 if __name__ == '__main__':
     app.run(debug=True)
+
